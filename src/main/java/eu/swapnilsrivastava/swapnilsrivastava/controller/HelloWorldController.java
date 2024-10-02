4f5d2d79ca2342c72120a5a2e9602f986c696a41
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-10-02 01:49:03 +0000

    adding message endpoint mapping sending just the message

--- src/main/java/eu/swapnilsrivastava/swapnilsrivastava/controller/HelloWorldController.java
+++ src/main/java/eu/swapnilsrivastava/swapnilsrivastava/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package eu.swapnilsrivastava.swapnilsrivastava.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class HelloWorldController {
     @GetMapping("/hello")
     public String Hello() {
         return "Hello Hello";
+    }
+
+    @GetMapping(value = "/message", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getMessage() {
+        return "Hello from the microservice!";
     }
     
 }
