9a67fafb802261b5c06adf351bdf5dd83ff99212
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-10-02 02:03:55 +0000

    changing the working value in the return for gcp

--- src/main/java/eu/swapnilsrivastava/swapnilsrivastava/controller/HelloWorldController.java
+++ src/main/java/eu/swapnilsrivastava/swapnilsrivastava/controller/HelloWorldController.java
@@ -10,22 +10,22 @@ public class HelloWorldController {
 
     @GetMapping("/hello-gcp")
     public String HelloWorldSprings() {
-       return "hello world google cloud platform";
+       return "hello world google cloud platform :: Working";
     }
 
     @GetMapping("/hello-world")
     public String HelloWorld() {
-        return "Hello world";
+        return "Hello world :: Working";
     }
 
     @GetMapping("/hello")
     public String Hello() {
-        return "Hello Hello";
+        return "Hello Hello :: Working";
     }
 
     @GetMapping(value = "/message", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getMessage() {
-        return "Hello from the microservice!";
+        return "Hello from the microservice! :: Working";
     }
     
 }
