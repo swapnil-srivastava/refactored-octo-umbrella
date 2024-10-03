779ed7fafa7cf91e4f22df15f86963ada15011e8
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-10-03 12:48:47 +0000

    adding the component , data and no argscontructor

--- src/main/java/eu/swapnilsrivastava/swapnilsrivastava/models/HelloWorldModel.java
+++ src/main/java/eu/swapnilsrivastava/swapnilsrivastava/models/HelloWorldModel.java
@@ -1,13 +1,20 @@
 package eu.swapnilsrivastava.swapnilsrivastava.models;
 
+import org.springframework.stereotype.Component;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Component
 @Entity
 @Table(name = "hello")
+@Data
+@NoArgsConstructor
 public class HelloWorldModel {
     
     @Id
