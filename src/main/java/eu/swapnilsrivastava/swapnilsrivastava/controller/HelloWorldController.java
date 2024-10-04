dbc56fe03ed9d29234017b1d4c5ce7e893b9c95a
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-10-04 00:21:38 +0000

    making changes to the endpoint

--- src/main/java/eu/swapnilsrivastava/swapnilsrivastava/controller/HelloWorldController.java
+++ src/main/java/eu/swapnilsrivastava/swapnilsrivastava/controller/HelloWorldController.java
@@ -15,22 +15,22 @@ public class HelloWorldController {
 
     @GetMapping("/hello-gcp")
     public String HelloWorldSprings() {
-       return "hello world google cloud platform :: Working :: added dependencies";
+       return "hello world google cloud platform :: Working";
     }
 
     @GetMapping("/hello-world")
     public String HelloWorld() {
-        return "Hello world :: Working :: added dependencies";
+        return "Hello world :: Working";
     }
 
     @GetMapping("/hello")
     public String Hello() {
-        return "Hello Hello :: Working :: added dependencies :: making changes";
+        return "Hello Hello :: Working";
     }
 
     @GetMapping(value = "/message", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getMessage() {
-        return "Hello from the microservice! :: Working :: added dependencies";
+        return "Hello from the microservice! :: Working";
     }
 
     @GetMapping("/call-heroku-service")
