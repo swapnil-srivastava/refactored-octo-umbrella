80ed2b4a79b6a4f7df2d07b47819721bd80f8027
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-10-05 20:19:55 +0000

    adding the web client to call the heroku microservice endpoint

--- src/main/java/eu/swapnilsrivastava/swapnilsrivastava/controller/HelloWorldController.java
+++ src/main/java/eu/swapnilsrivastava/swapnilsrivastava/controller/HelloWorldController.java
@@ -1,17 +1,26 @@
 package eu.swapnilsrivastava.swapnilsrivastava.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 public class HelloWorldController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient webClient;
 
     @GetMapping("/hello-gcp")
     public String HelloWorldSprings() {
@@ -34,13 +43,16 @@ public class HelloWorldController {
     }
 
     @GetMapping("/call-heroku-service")
-    public ResponseEntity<String> callHerokuService() {
-        String herokuService2Url = "https://spring-hackfestival2024-df62fb596841.herokuapp.com/call-heroku-service";
-        
-        ResponseEntity<String> response = restTemplate.getForEntity(herokuService2Url, String.class);
-        String message = response.getBody();
-        
-        return ResponseEntity.ok("Message from Heroku Service: " + message);
+    public Mono<ResponseEntity<String>> callGcpService() {
+        return webClient.get()
+            .uri("/call-heroku-service")
+            .retrieve()
+            .onStatus(HttpStatusCode::is4xxClientError, 
+                response -> Mono.error(new RuntimeException("4xx error")))
+            .onStatus(HttpStatusCode::is5xxServerError, 
+                response -> Mono.error(new RuntimeException("5xx error")))
+            .toEntity(String.class)
+            .timeout(Duration.ofSeconds(10));
     }
 
     @GetMapping("/call-guess-number-service")
