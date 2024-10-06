8e77a9a15849a472153c573e981d8aa57c78ee36
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-10-06 19:49:30 +0000

    adding the enable kafka

--- src/main/java/eu/swapnilsrivastava/swapnilsrivastava/SwapnilsrivastavaApplication.java
+++ src/main/java/eu/swapnilsrivastava/swapnilsrivastava/SwapnilsrivastavaApplication.java
@@ -2,8 +2,10 @@ package eu.swapnilsrivastava.swapnilsrivastava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class SwapnilsrivastavaApplication {
 
 	public static void main(String[] args) {
