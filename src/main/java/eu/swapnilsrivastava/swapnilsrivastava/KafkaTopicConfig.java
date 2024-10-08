12ea449f254087648aee5c7970aa501c753b0b1d
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-10-08 09:23:16 +0000

    addin sasl mechanism and config

--- src/main/java/eu/swapnilsrivastava/swapnilsrivastava/KafkaTopicConfig.java
+++ src/main/java/eu/swapnilsrivastava/swapnilsrivastava/KafkaTopicConfig.java
@@ -12,16 +12,30 @@ import org.springframework.kafka.core.KafkaAdmin;
 
 @Configuration
 public class KafkaTopicConfig {
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
+
+    @Value("${spring.kafka.bootstrap-servers}") 
+    private String bootstrapAddress;
+  
+    @Value("${spring.kafka.properties.sasl.mechanism}") 
+      private String saslMechanism;
+    
+    
+    @Value("${spring.kafka.properties.sasl.jaas.config}") 
+      private String salsJaasConfig;
+  
+    @Value("${spring.kafka.properties.security.protocol}") 
+      private String securityProtocol;
 
     @Bean 
     public KafkaAdmin admin() {
       return new KafkaAdmin(Map.of(
-        AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
+        AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress,
+        "**sasl.mechanism**", saslMechanism,
+        "**sasl.jaas.config**", salsJaasConfig,
+        "**security.protocol**", securityProtocol,
         AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, "30000",
         AdminClientConfig.DEFAULT_API_TIMEOUT_MS_CONFIG, "60000"
-      ));
+        ));
     }
 
     @Bean
