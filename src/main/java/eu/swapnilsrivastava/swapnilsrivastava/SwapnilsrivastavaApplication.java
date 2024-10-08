1d8a2204bf95e95e8fe16fdaf93c818fbb0989a3
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-10-08 14:29:03 +0000

    adding kafka streams and kafka stream processor

--- src/main/java/eu/swapnilsrivastava/swapnilsrivastava/SwapnilsrivastavaApplication.java
+++ src/main/java/eu/swapnilsrivastava/swapnilsrivastava/SwapnilsrivastavaApplication.java
@@ -3,9 +3,11 @@ package eu.swapnilsrivastava.swapnilsrivastava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 @SpringBootApplication
 @EnableKafka
+@EnableKafkaStreams
 public class SwapnilsrivastavaApplication {
 
 	public static void main(String[] args) {
